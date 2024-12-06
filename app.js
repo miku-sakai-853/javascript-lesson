e083c358ba7636af315822faf79b558f988af9a7
Miku Sakai <sakai.960530@gmail.com>
2024-12-06 16:02:44 +0900

    応用編問題回答

--- app.js
+++ app.js
@@ -68,14 +68,17 @@ calc.add = function(x, y) {
   console.log(x + y);
 }
 calc.add(3, 4);
+
 calc.subtract = function(x, y) {
   console.log(x - y);
 }
 calc.subtract(15, 5);
+
 calc.multiply = function(x, y) {
   console.log(x * y);
 }
 calc.multiply(7, 7);
+
 calc.divide = function(x, y) {
   console.log(x / y);
 }
@@ -88,8 +91,46 @@ function remainder(x, y) {
 console.log('5 を 3 で割った余りは ' + remainder(5, 3) + ' です。');
 
 //Q10 スコープ
-function foo() {
-  let x = 1;
-}
-console.log(x);
 //関数のスコープに定義された変数は、スコープの内側でのみ参照できる。その為、スコープの外側にあるconsole.log(x)からは変数xを参照できずエラーが出力される。
+
+
+
+//応用編 問題
+//Q1 標準組み込みオブジェクト
+let random = Math.floor(Math.random() * 9) + 1;
+console.log('random => ', random);
+
+//Q2 コールバック関数
+function hello() {
+    console.log('Hello World!');
+}
+setTimeout(hello, 3000);
+
+//Q3 if
+let num = 0;
+if (num > 0) {
+  console.log('num is greater than 0');
+} else if(num < 0) {
+  console.log('num is less than 0')
+} else {
+  console.log('num is 0')
+}
+
+//Q4 for
+let numbers = [];
+for (let i = 0; i <= 99; i++) {
+  numbers = numbers + i;
+}
+console.log(numbers);
+
+//Q5 for × if
+let mixed = [4, '2', 5, '8', '9', 0, 1];
+for (let i = 0; i < mixed.length; i++) {
+  if (Number.isFinite(mixed[i]) && mixed[i] % 2 == 0) {
+    console.log('even');
+  } else if (Number.isFinite(mixed[i]) && mixed[i] % 2 == 1) {
+    console.log('odd');
+  } else {
+    console.log('not number');
+  }
+}
